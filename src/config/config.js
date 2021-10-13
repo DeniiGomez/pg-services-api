d07c6b6433a3c6afc7ce923a0b995207577c198c
brandon gomez <denii.27.g0@gmail.com>
2021-10-13 01:47:25 -0600

    producction env

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME || "root",
-    "password": process.env.DB_PASSWORD || "my_secret",
-    "database": process.env.DB_DATABSE || "test_login",
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_CONNECTION || "mysql" || "postgres"
+    "username": "root",
+    "password": "my_secret",
+    "database": "test_login",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME || "root",
+    "password": process.env.DB_PASSWORD || "my_secret",
+    "database": process.env.DB_DATABSE || "test_login",
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_CONNECTION || "mysql" || "postgres",
   }
 }
